b0855749385f513a68e859b0a16ecaae84390cba
rob@biza.co.za <robpicotti@gmail.com>
2021-01-27 14:07:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace TAD_run_portfolio_update
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "cmd.exe";
+            info.RedirectStandardInput = true;
+            info.UseShellExecute = false;
+            p.StartInfo = info;
+            p.Start();
+            using (StreamWriter sw = p.StandardInput)
+            {
+                if (sw.BaseStream.CanWrite)
+                {
+                    sw.WriteLine("conda activate trading_prod");
+                    sw.WriteLine("python run_portfolio_update.py");
+                }
+            }
+        }
+    }
+}
